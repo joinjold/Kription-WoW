9efea18509bf150b231455ab9f249a9e2c48310b
joinjold <593966772@qq.com>
2017-04-21 10:07:45 +0800

    修改决斗重置代码

--- src/server/scripts/Custom/Duel_Reset.cpp
+++ src/server/scripts/Custom/Duel_Reset.cpp
@@ -1,36 +1,20 @@
-#include "ScriptMgr.h"
-#include "Cell.h"
-#include "CellImpl.h"
-#include "GameEventMgr.h"
-#include "GridNotifiers.h"
-#include "GridNotifiersImpl.h"
-#include "Unit.h"
-#include "GameObject.h"
-#include "ScriptedCreature.h"
-#include "ScriptedGossip.h"
-#include "InstanceScript.h"
-#include "CombatAI.h"
-#include "PassiveAI.h"
-#include "Chat.h"
-#include "DBCStructure.h"
-#include "DBCStores.h"
-#include "ObjectMgr.h"
-#include "SpellScript.h"
-#include "SpellAuraEffects.h"
+#include "ScriptPCH.h"
  
 class Duel_Reset : public PlayerScript
 {
         public:
                 Duel_Reset() : PlayerScript("Duel_Reset"){}
  
-                void OnDuelEnd(Player* Plr, Player* Plr2, DuelCompleteType /*type*/)
+                void OnDuelEnd(Player* pWinner, Player* pLoser, DuelCompleteType /*type*/)
                 {
-                        Plr->SetHealth(Plr->GetMaxHealth());
-                        Plr->RemoveArenaSpellCooldowns();
-                        Plr->ResetAllPowers();
-                        Plr2->SetHealth(Plr2->GetMaxHealth());
-                        Plr2->RemoveArenaSpellCooldowns();
-                        Plr2->ResetAllPowers();
+                        pWinner->RemoveAllSpellCooldown();
+                        pLoser->RemoveAllSpellCooldown();
+                        pWinner->SetHealth(pWinner->GetMaxHealth());
+                        if ( pWinner->getPowerType() == POWER_MANA )
+                                pWinner->SetPower(POWER_MANA, pWinner->GetMaxPower(POWER_MANA));
+                        pLoser->SetHealth(pLoser->GetMaxHealth());
+                        if ( pLoser->getPowerType() == POWER_MANA )
+                                pLoser->SetPower(POWER_MANA,  pLoser->GetMaxPower(POWER_MANA));
                 }
 };
  
